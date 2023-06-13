d1403002ced64e452eaaa084c9ac4724d1a7ba78
RamiroBertran <ramirobertra@gmail.com>
2023-06-13 18:52:20 -0300

    feat: add function that call itself in case no argument is given

--- element.sh
+++ element.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 PSQL="psql -X --username=kvothe_snow --dbname=periodic_table --no-align -t -c"
-PROVIDE_ARGUMENT() {
+echo "Please provide an element as an argument"
+
+GET_ELEMENT_INFORMATION() {
   if [[ $1 ]]
   then 
-    echo -e "\nPlease provide an element as an argument"
+    echo "$1"
+  fi
+  read ELEMENT
+  if [[ -z $ELEMENT ]] 
+  then
+    GET_ELEMENT_INFORMATION "Please provide an element as an argument"
   else 
-    echo $1
+    echo -e "$ELEMENT"
   fi
 }
 
-PROVIDE_ARGUMENT
+GET_ELEMENT_INFORMATION
