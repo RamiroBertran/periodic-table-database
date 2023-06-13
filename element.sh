8c180572b6e1489a17aa0884367ec3751718f92d
RamiroBertran <ramirobertra@gmail.com>
2023-06-13 20:23:54 -0300

    feat: add querys to get more information about elemetns

--- element.sh
+++ element.sh
@@ -16,14 +16,40 @@ GET_ELEMENT_INFORMATION() {
     if [[ $ELEMENT =~ [0-9]+ ]]
     then 
       # Since element is a number: 
-
+      # check if the element is in database 
       # get atomic_number
       ATOMIC_NUMBER="$($PSQL "SELECT atomic_number FROM properties JOIN elements USING(atomic_number) WHERE (atomic_number=$ELEMENT)")"
+      if [[ -z ATOMIC_NUMBER ]]
+      then
+        GET_ELEMENT_INFORMATION "Sorry, we don't find any ralation in our database"
+        else 
       # get name 
-      NAME="$($PSQL "SELECT name FROM elements JOIN properites USING(atomic_number) WHERE (atomic_number=$ELEMENT)")"
+      NAME="$($PSQL "SELECT name FROM elements JOIN properties USING(atomic_number) WHERE (atomic_number=$ELEMENT)")"
+      # get type 
+      TYPE="$($PSQL "SELECT type FROM properties JOIN types USING(type_id) WHERE (atomic_number=$ELEMENT)")"
+      # get atomic_mass
+      ATOMIC_MASS="$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$ELEMENT")"
+      # get melting_point_celsius
+      MELTING_POINT="$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$ELEMENT")"
+      # get boiling_point_celsius
+      BOILING_POINT="$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$ELEMENT")"
+      fi
       else 
-      echo "It's a string"
-      # Since element is not a number
+      # Since element is not a number:
+      # get atomic_number
+      ATOMIC_NUMBER="$($PSQL "SELECT atomic_number FROM properties JOIN elements USING(atomic_number) WHERE (symbol='$ELEMENT' OR name='$ELEMENT')")"
+      if [[ -z $ATOMIC_NUMBER ]]
+      then 
+        GET_ELEMENT_INFORMATION "Sorry we do not find any relation in our database"
+        else
+      #get type 
+      TYPE="$($PSQL "SELECT type FROM properties JOIN types USING(type_id) JOIN elements USING(atomic_number) WHERE (symbol='$ELEMENT' OR name='$ELEMENT')")"
+      #get atomic_mass 
+      ATOMIC_MASS="$($PSQL "SELECT atomic_mass FROM properties JOIN elements USING(atomic_number) WHERE (symbol='$ELEMENT' OR name='$ELEMENT')")"
+      #get melting_point_celsius
+      MELTING_POINT="$($PSQL "SELECT melting_point_celsius FROM properties JOIN elements USING(atomic_number) WHERE (symbol='$ELEMENT' OR name='$ELEMENT')")"
+      #get boliling_point_celsius 
+      BOILING_POINT="$($PSQL "SELECT boiling_point_celsius FROM properties JOIN elements USING(atomic_number) WHERE (symbol='$ELEMENT' OR name='$ELEMENT')")"
       #get length of string
       ELEMENT_LENGHT=$(echo -n "$ELEMENT" | wc -m)
       if [[ $ELEMENT_LENGHT -gt 2 ]] 
@@ -36,8 +62,7 @@ GET_ELEMENT_INFORMATION() {
       # query to get the name
       NAME="$($PSQL "SELECT name FROM elements JOIN properties USING(atomic_number) WHERE (symbol='$ELEMENT')")"
       fi
-      # get atomic_number
-      ATOMIC_NUMBER="$($PSQL "SELECT atomic_number FROM properties JOIN elements USING(atomic_number) WHERE (symbol='$ELEMENT' OR name='$ELEMENT')")"
+      fi
     fi
   fi
 }
